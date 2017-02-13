55aa152933f94d92ff01880766dccde3d04a0c7e
datamaning <george_xl@163.com>
2017-02-13 20:16:09 +0800

    add removal

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -15,5 +15,12 @@ data = featureFormat(data_dict, features)
 
 ### your code below
 
+from sklearn import linear_model
 
+clf=linear_model.LinearRegression()
+print data
+print data[:,0]
+print len(data[:,0]),len(data[:,1])
+clf.fit(data,[0])
+print clf.coef_
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-
+from pprint import pprint
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
@@ -14,7 +14,13 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
-
-    
+    for i in range(len(predictions)):
+        error=(predictions[i]-net_worths[i])*(predictions[i]-net_worths[i])
+        data=(ages[i],net_worths[i],error)
+        cleaned_data.append(data)
+    cleaned_data=sorted(cleaned_data,key=lambda x:x[2])
+    num=int(len(predictions)*0.9)
+    cleaned_data=cleaned_data[0:num]
+    print(cleaned_data),len(cleaned_data)
     return cleaned_data
 
