4cca9eb658503e79c4a393d0030710495c81f1be
datamaning <george_xl@163.com>
2017-02-15 00:07:59 +0800

    fix data

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -20,14 +20,16 @@ authors = pickle.load( open(authors_file, "r") )
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
 from sklearn import cross_validation
+#print word_data,"w"
 features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
+
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
-
+words=vectorizer.get_feature_names()
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
@@ -36,12 +38,25 @@ features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
 
-from sklearn import DecisionTree
-
-clf=DecisionTree()
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf=DecisionTreeClassifier(min_samples_split=40)
 clf.fit(features_train,labels_train)
-print clf.score(features_test,labels_test)
+pred=clf.predict(features_test)
+print 'accuracy score',accuracy_score(labels_test,pred)
 ### your code goes here
 
+print clf.feature_importances_
+for index,feature in enumerate(clf.feature_importances_):
+    if feature>0.2:
+        print 'feature no',index
+        print 'importance',feature
+        print 'word',words[index]
 
+vectorizer=TfidVectorizer(stop_words='english')
+vectorizer.fit(word_data)
+vectorizer.transform(word_data)
 
+feature_words=vectorizer.get_feature_names()
+print 'number of words',len(feature_words)
+print 'word number 34957',feature_words[34957]
