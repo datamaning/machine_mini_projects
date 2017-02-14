9ac9b9539e8d7c6bf76ba359ccb46948f1f28e30
datamaning <george_xl@163.com>
2017-02-14 22:23:52 +0800

    add new

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,18 +44,23 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            #print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text=parseOutText(email)
+            text=text.replace('sara','')
+            if temp_counter==1:
+                print text
+            #print name
             ### use str.replace() to remove any instances of the words
+
             ### ["sara", "shackleton", "chris", "germani"]
 
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(1)
 
             email.close()
 
