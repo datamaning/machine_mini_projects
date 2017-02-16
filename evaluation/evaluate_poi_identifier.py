9f0210904371e3fd9f995c42f2bdea2f670cc98e
datamaning <george_xl@163.com>
2017-02-16 20:03:41 +0800

    evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,9 +13,11 @@
 
 import pickle
 import sys
+import numpy as np
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+from sklearn.cross_validation import train_test_split
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
 ### add more features to features_list!
@@ -24,8 +26,16 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+from sklearn.tree import DecisionTreeClassifier
+features_train,features_test,labels_train,labels_test=train_test_split(features, labels, random_state=42, test_size=0.3)
+clf=DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
+print clf.score(features_test,labels_test)
 
-
+values,counts=np.unique(pred,return_counts=True)
+test_size=len(features_test)
+print "predicted of POIDS",zip(values,counts)
 ### your code goes here 
 
 
